8e73b35117a1f0d585c65e40acda4da53dd28e39
Denys Solianyk <peacemaker@ukr.net>
2013-05-13 18:48:45 +0300

    Spring dummy example

--- src/main/java/impl/Console.java
+++ src/main/java/impl/Console.java
@@ -1,9 +1,13 @@
 package impl;
 
+import generator.utils.filesystem.file.scanner.impl.FilesScanner;
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.spi.MapOptionHandler;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -17,10 +21,12 @@ import java.util.Map;
  */
 public class Console {
 
-    @Argument(metaVar = "[target [target2 [target3] ...]]", usage = "targets")
+    @Argument(metaVar = "[target [target2 [target3] ...]]",
+            usage = "targets")
     private List<String> targets = new ArrayList<String>();
 
-    @Option(name = "-h", aliases = "-help", usage = "print this message")
+    @Option(name = "-h", aliases = "-help",
+            usage = "print this message")
     private boolean help = false;
 
     @Option(name = "-lib", metaVar = "<path>",
@@ -36,23 +42,15 @@ public class Console {
                     + "1 (lowest) to 10 (highest); 5 is the default")
     private int nice = 5;
 
-    private Map<String, String> properties = new HashMap<String, String>();
-
     @Option(name = "-D", metaVar = "<property>=<value>",
-            usage = "use value for given property")
-    private void setProperty(final String property) throws CmdLineException {
-        String[] arr = property.split("=");
-        if (arr.length != 2) {
-            throw new CmdLineException("Properties must be specified in the form:" +
-                    "<property>=<value>");
-        }
-        properties.put(arr[0], arr[1]);
-    }
+            usage = "use value for given property",
+            handler = MapOptionHandler.class) //MyMapOptionHandler.class
+    private Map<String, String> properties = new HashMap<String, String>();
 
     public static void main(String[] args) throws CmdLineException {
 
-        final String[] argv = {"-D", "key=value", "-f", "build.xml",
-                "-D", "key2=value2", "clean", "install"};
+        final String[] argv = {"test", "-D", "key = value", "-f", "build.xml",
+                "-D", "key2 =' value2'", "clean", "install"};
         final Console options = new Console();
         final CmdLineParser parser = new CmdLineParser(options);
         parser.parseArgument(argv);
@@ -60,6 +58,15 @@ public class Console {
         // print usage
         parser.setUsageWidth(Integer.MAX_VALUE);
         parser.printUsage(System.err);
+
+        System.out.println(options.targets);
+        System.out.println(options.properties);
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("console.xml");
+        FilesScanner filesScanner = (FilesScanner) context.getBean("filesScanner");
+
+        filesScanner.execute();
+        //obj.getMessage();
 
         // check the options have been set correctly
         //assertEquals("build.xml", options.buildFile.getName());
